e05de53a3f326dd4e66b6aaa98c242cad0422cc2
nick-bytee <nikhilkaushik2228@gmail.com>
2024-05-24 17:31:15 +0530

    base project with header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/UI/Header/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
